2b1179469fd28eddf11d0dcb10bc86614ca3f91f
Jason Cruz <jac024@ucsd.edu>
2022-10-13 07:25:46 -0700

    merge mock up testing, WIP

--- merge.go
+++ merge.go
@@ -1,15 +1,98 @@
 package neo4j
 
 import (
+	"errors"
 	"fmt"
+	"log"
+	"strings"
 
 	"github.com/cloudprivacylabs/lpg"
 	"github.com/cloudprivacylabs/lsa/pkg/ls"
 	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
 )
 
-func merge(g1, g2 *lpg.Graph) (*lpg.Graph, error) {
-	return nil, nil
+func compareNativeNode(n1, n2 *lpg.Node) bool {
+	if !n1.GetLabels().IsEqual(n2.GetLabels()) {
+		return false
+	}
+	// Expected properties must be a subset
+	propertiesOK := true
+	n2.ForEachProperty(func(k string, v interface{}) bool {
+		pv, ok := v.(*ls.PropertyValue)
+		if !ok {
+			return true
+		}
+		v2, ok := n1.GetProperty(k)
+		if !ok {
+			log.Printf("Error at %s: %v: Property does not exist", k, v)
+			propertiesOK = false
+			return false
+		}
+		pv2, ok := v2.(*ls.PropertyValue)
+		if !ok {
+			log.Printf("Error at %s: %v: Not property value", k, v)
+			propertiesOK = false
+			return false
+		}
+		if !pv2.IsEqual(pv) {
+			if strings.ToLower(pv2.AsString()) != strings.ToLower(pv.AsString()) {
+				log.Printf("Error at %s: Got %v, Expected %v: Values are not equal", k, pv, pv2)
+				propertiesOK = false
+				return false
+			}
+		}
+		return true
+	})
+	if !propertiesOK {
+		log.Printf("Properties not same")
+		return false
+	}
+	log.Printf("True\n")
+	return true
+}
+
+func compareNativeEdge(e1, e2 *lpg.Edge) bool {
+	return e1.GetLabel() == e2.GetLabel() && ls.IsPropertiesEqual(ls.PropertiesAsMap(e1), ls.PropertiesAsMap(e2))
+}
+
+func merge(memGraph, dbGraph *lpg.Graph, memGraphIds, dbGraphIds map[*lpg.Node]int64, memEdges, dbEdges map[*lpg.Edge]int64) (*lpg.Graph, []string, error) {
+	grph := lpg.NewGraph()
+	ops := make([]string, 0)
+
+	for memNode, _ := range memGraphIds {
+		memEdgeItr := memNode.GetEdges(lpg.AnyEdge)
+		memEdge := memEdgeItr.Edge()
+		memParent := memEdgeItr.Edge().GetFrom()
+		// memChild := memEdgeItr.Edge().GetTo()
+		for dbNode, _ := range dbGraphIds {
+			dbEdgeItr := dbNode.GetEdges(lpg.AnyEdge)
+			dbEdge := dbEdgeItr.Edge()
+			dbParent := dbEdgeItr.Edge().GetFrom()
+			// dbChild := dbEdgeItr.Edge().GetTo()
+			p1, ok := memNode.GetProperty(ls.SchemaNodeIDTerm)
+			if !ok {
+				return nil, nil, errors.New("must have schema node id term")
+			}
+			p2, ok := dbNode.GetProperty(ls.SchemaNodeIDTerm)
+			if !ok {
+				return nil, nil, errors.New("must have schema node id term")
+			}
+			if ls.AsPropertyValue(p1, true).AsString() == ls.AsPropertyValue(p2, true).AsString() {
+				if memParent.GetLabels().IsEqual(dbParent.GetLabels()) && compareNativeEdge(memEdge, dbEdge) {
+					cmp := compareNativeNode(memNode, dbNode)
+					if !cmp {
+						ops = append(ops, writeQuery(memNode, dbGraphIds[dbNode]))
+					}
+				}
+			}
+		}
+	}
+
+	return grph, ops, nil
+}
+
+func writeQuery(n *lpg.Node, id int64) string {
+	return fmt.Sprintf("MERGE (n)")
 }
 
 func loadGraphByEntities(tx neo4j.Transaction, grph *lpg.Graph, rootIds []uint64, config Config, loadNeighbors func(neo4j.Transaction, []uint64) ([]neo4jNode, []neo4jNode, []neo4jEdge, error), selectEntity func(*lpg.Node) bool) (map[*lpg.Node]int64, error) {
--- merge_test.go
+++ merge_test.go
@@ -2,14 +2,10 @@ package neo4j
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
-	"reflect"
-	"testing"
 
 	"github.com/cloudprivacylabs/lpg"
 	"github.com/cloudprivacylabs/lsa/pkg/ls"
-	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
 )
 
 type dbUpdateNode struct {
@@ -18,122 +14,157 @@ type dbUpdateNode struct {
 	properties map[string]interface{}
 }
 
-func (n dbUpdateNode) writeQuery() string {
-	return ""
-}
-
 // in db, ls.SchemaNodeIDTerm is "schemaNodeId"
-func findNode(tx neo4j.Transaction, id int64, schemaNodeId string, parent, child *lpg.Node) (neo4j.Node, error) {
-	idrec, err := tx.Run(fmt.Sprintf("MATCH (n) WHERE ID(n) = %d RETURN n", id), map[string]interface{}{})
+// func findNode(tx neo4j.Transaction, id int64, schemaNodeId string, parent, child *lpg.Node) (neo4j.Node, error) {
+// 	idrec, err := tx.Run(fmt.Sprintf("MATCH (n) WHERE ID(n) = %d RETURN n", id), map[string]interface{}{})
+// 	if err != nil {
+// 		return neo4j.Node{}, err
+// 	}
+// 	rec, err := idrec.Collect()
+// 	if err != nil {
+// 		return neo4j.Node{}, err
+// 	}
+// 	if len(rec) == 0 {
+// 		sidrec, err := tx.Run(fmt.Sprintf("MATCH (n) WHERE n.`%s` = %s RETURN n", ls.SchemaNodeIDTerm, quoteStringLiteral(schemaNodeId)), map[string]interface{}{})
+// 		if err != nil {
+// 			return neo4j.Node{}, err
+// 		}
+// 		for sidrec.Next() {
+// 			r := sidrec.Record()
+// 		}
+// 	}
+// 	return rec[0].Values[0].(neo4j.Node), nil
+// }
+
+// func compareNativeNodeToDBNode(n1 *lpg.Node, n2 neo4j.Node) bool {
+// 	if !reflect.DeepEqual(n1.GetLabels().Slice(), n2.Labels) {
+// 		return false
+// 	}
+// 	eq := n1.ForEachProperty(func(s string, i interface{}) bool {
+// 		if prop, ok := n2.Props[s]; ok {
+// 			if !reflect.DeepEqual(prop, i) {
+// 				return false
+// 			}
+// 			return true
+// 		}
+// 		return true
+// 	})
+// 	if !eq {
+// 		return false
+// 	}
+// 	return true
+// }
+
+// testGraphMerge (without DB)
+func testGraphMerge(memGraphFile, dbGraphFile string) (*lpg.Graph, []string, error) {
+	dbGraph, dbNodeIds, dbEdgeIds, err := mockLoadGraph(dbGraphFile)
 	if err != nil {
-		return neo4j.Node{}, err
+		return nil, nil, err
 	}
-	rec, err := idrec.Collect()
+	memGraph, memNodeIds, memEdgeIds, err := mockLoadGraph(memGraphFile)
 	if err != nil {
-		return neo4j.Node{}, err
+		return nil, nil, err
 	}
-	if len(rec) == 0 {
-		sidrec, err := tx.Run(fmt.Sprintf("MATCH (n) WHERE n.`%s` = %s RETURN n", ls.SchemaNodeIDTerm, quoteStringLiteral(schemaNodeId)), map[string]interface{}{})
-		if err != nil {
-			return neo4j.Node{}, err
-		}
-		for sidrec.Next() {
-			r := sidrec.Record()
-		}
-	}
-	return rec[0].Values[0].(neo4j.Node), nil
+	return merge(memGraph, dbGraph, dbNodeIds, memNodeIds, memEdgeIds, dbEdgeIds)
 }
 
-func compareNativeNodeToDBNode(n1 *lpg.Node, n2 neo4j.Node) bool {
-	if !reflect.DeepEqual(n1.GetLabels().Slice(), n2.Labels) {
-		return false
-	}
-	eq := n1.ForEachProperty(func(s string, i interface{}) bool {
-		if prop, ok := n2.Props[s]; ok {
-			if !reflect.DeepEqual(prop, i) {
-				return false
-			}
-			return true
-		}
-		return true
-	})
-	if !eq {
-		return false
-	}
-	return true
-}
-
-func TestMerge(t *testing.T) {
-	var session *Session
-	var driver neo4j.Driver
-	var cfg Config
-
-	drv := NewDriver(driver, "neo4j")
-	session = drv.NewSession()
-	defer session.Close()
-	tx, err := session.BeginTransaction()
+func mockLoadGraph(filename string) (*lpg.Graph, map[*lpg.Node]int64, map[*lpg.Edge]int64, error) {
+	f, err := os.Open(filename)
 	if err != nil {
-		t.Error(err)
+		return nil, nil, nil, err
 	}
-
-	f, err := os.Open("examples/merge_02.json")
-	if err != nil {
-		t.Error(err)
-	}
-	g1 := lpg.NewGraph()
+	grph := lpg.NewGraph()
 	m := ls.JSONMarshaler{}
-	if err := m.Decode(g1, json.NewDecoder(f)); err != nil {
-		t.Error(err)
+	if err := m.Decode(grph, json.NewDecoder(f)); err != nil {
+		return nil, nil, nil, err
 	}
-
-	ids, err := loadGraphByEntities(tx, lpg.NewGraph(), nil, cfg, findNeighbors, selectEntity)
-	if err != nil {
-		t.Error(err)
-	}
-	// find difference between in-mem and db nodes
-	for nodeItr := g1.GetNodes(); nodeItr.Next(); {
+	var ix int
+	mockNodeIDs := make(map[*lpg.Node]int64)
+	for nodeItr := grph.GetNodes(); nodeItr.Next(); ix++ {
 		node := nodeItr.Node()
-		p, ok := node.GetProperty(ls.SchemaNodeIDTerm)
-		if !ok {
-			t.Errorf("must have schema node id term")
-		}
-		edgeItr := node.GetEdges(lpg.AnyEdge)
-		parent := edgeItr.Edge().GetFrom()
-		child := edgeItr.Edge().GetTo()
-		cmpNode, err := findNode(tx, ids[node], ls.AsPropertyValue(p, true).AsString(), parent, child)
-		if err != nil {
-			t.Error(err)
-		}
-
+		node.SetProperty(ls.NodeIDTerm, ix)
+		mockNodeIDs[node] = int64(ix)
 	}
-
-	exp := lpg.NewGraph()
-	m = ls.JSONMarshaler{}
-	f, err = os.Open("examples/merge_03.json")
-	if err != nil {
-		t.Error(err)
+	ix = 0
+	mockEdgeIDs := make(map[*lpg.Edge]int64)
+	for edgeItr := grph.GetEdges(); edgeItr.Next(); ix++ {
+		edge := edgeItr.Edge()
+		mockEdgeIDs[edge] = int64(ix)
 	}
-	if err := m.Decode(exp, json.NewDecoder(f)); err != nil {
-		t.Error(err)
-	}
-
-	// for nodeItr := exp.GetNodes(); nodeItr.Next(); {
-	// 	node := nodeItr.Node()
-	// 	if node.HasLabel("test") {
-	// 		n := dbUpdateNode{
-	// 			nodeID:     ids[node],
-	// 			labels:     node.GetLabels().Slice(),
-	// 			properties: make(map[string]interface{}),
-	// 		}
-	// 		node.ForEachProperty(func(s string, i interface{}) bool {
-	// 			n.properties[s] = i
-	// 			return true
-	// 		})
-	// 		q := n.writeQuery()
-	// 		if q != "" {
-	// 			t.Errorf("invalid query")
-	// 		}
-	// 	}
-	// }
-
+	return grph, mockNodeIDs, mockEdgeIDs, nil
 }
+
+// func TestMerge(t *testing.T) {
+// 	var session *Session
+// 	var driver neo4j.Driver
+// 	var cfg Config
+
+// 	drv := NewDriver(driver, "neo4j")
+// 	session = drv.NewSession()
+// 	defer session.Close()
+// 	tx, err := session.BeginTransaction()
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+
+// 	f, err := os.Open("examples/merge_02.json")
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+// 	g1 := lpg.NewGraph()
+// 	m := ls.JSONMarshaler{}
+// 	if err := m.Decode(g1, json.NewDecoder(f)); err != nil {
+// 		t.Error(err)
+// 	}
+
+// 	ids, err := loadGraphByEntities(tx, lpg.NewGraph(), nil, cfg, findNeighbors, selectEntity)
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+// 	// find difference between in-mem and db nodes
+// 	for nodeItr := g1.GetNodes(); nodeItr.Next(); {
+// 		node := nodeItr.Node()
+// 		p, ok := node.GetProperty(ls.SchemaNodeIDTerm)
+// 		if !ok {
+// 			t.Errorf("must have schema node id term")
+// 		}
+// 		edgeItr := node.GetEdges(lpg.AnyEdge)
+// 		parent := edgeItr.Edge().GetFrom()
+// 		child := edgeItr.Edge().GetTo()
+// 		cmpNode, err := findNode(tx, ids[node], ls.AsPropertyValue(p, true).AsString(), parent, child)
+// 		if err != nil {
+// 			t.Error(err)
+// 		}
+
+// 	}
+
+// 	exp := lpg.NewGraph()
+// 	m = ls.JSONMarshaler{}
+// 	f, err = os.Open("examples/merge_03.json")
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+// 	if err := m.Decode(exp, json.NewDecoder(f)); err != nil {
+// 		t.Error(err)
+// 	}
+
+// 	// for nodeItr := exp.GetNodes(); nodeItr.Next(); {
+// 	// 	node := nodeItr.Node()
+// 	// 	if node.HasLabel("test") {
+// 	// 		n := dbUpdateNode{
+// 	// 			nodeID:     ids[node],
+// 	// 			labels:     node.GetLabels().Slice(),
+// 	// 			properties: make(map[string]interface{}),
+// 	// 		}
+// 	// 		node.ForEachProperty(func(s string, i interface{}) bool {
+// 	// 			n.properties[s] = i
+// 	// 			return true
+// 	// 		})
+// 	// 		q := n.writeQuery()
+// 	// 		if q != "" {
+// 	// 			t.Errorf("invalid query")
+// 	// 		}
+// 	// 	}
+// 	// }
+
+// }
