8011e7cc4854b8f7e051cbbc4cc41b674a2e634e
Burak Serdar <brxd@cloudprivacylabs.com>
2023-01-20 09:35:07 -0700

    Remove initSession

--- lsaneo/cmd/utils.go
+++ lsaneo/cmd/utils.go
@@ -6,24 +6,8 @@ import (
 
 	neo "github.com/cloudprivacylabs/lsa-neo4j"
 	"github.com/cloudprivacylabs/lsa/layers/cmd/cmdutil"
-	"github.com/cloudprivacylabs/lsa/pkg/ls"
-	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 	"github.com/spf13/cobra"
 )
-
-func initSession(cmd *cobra.Command) (*ls.Context, neo4j.ExplicitTransaction, error) {
-	drv := getNeoDriver(cmd)
-	ctx := ls.DefaultContext()
-	session := drv.NewSession(ctx)
-	defer session.Close(ctx)
-	var tx neo4j.ExplicitTransaction
-	var err error
-	tx, err = session.BeginTransaction(ctx)
-	if err != nil {
-		return nil, nil, err
-	}
-	return ctx, tx, nil
-}
 
 func loadConfig(cmd *cobra.Command) (neo.Config, error) {
 	var cfg neo.Config
